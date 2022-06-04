2f51ef6bd373e9dd252d80222b040a0c3a8338ee
Matheus Farias Fernandes <matheus_farias_fernandes@outlook.com>
2022-06-04 20:44:10 -0300

    feat:Pagina de exibicao

--- Models/Resposta.cs
+++ Models/Resposta.cs
@@ -3,11 +3,11 @@ namespace Questionario.Models
 {
     public class Resposta
     {
-        [Required]
+        [Required(ErrorMessage ="O nome deve ser inserido")]
         public string Nome{get;set;}=string.Empty;
-        [Required]
+        [Required (ErrorMessage ="O Email deve ser inserido")]
         public string Email{get;set;}=string.Empty;
-        [Required]
+        [Required(ErrorMessage ="Uma opção deve ser marcada")]
         public bool? Sim{get;set;}
         
     }
